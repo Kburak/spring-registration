6317f8a280138ee61fa5cb28b276af31f0ef3d5f
burak.engin <kburakengin@gmail.com>
2021-01-10 22:24:55 +0300

    registration, logout, authentication completed

--- src/main/java/com/kburakengin/registerlogin/demo/service/UserService.java
+++ src/main/java/com/kburakengin/registerlogin/demo/service/UserService.java
@@ -2,7 +2,11 @@ package com.kburakengin.registerlogin.demo.service;
 
 import com.kburakengin.registerlogin.demo.dto.UserRegistrationDto;
 import com.kburakengin.registerlogin.demo.model.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
 
-public interface UserService {
+import java.util.List;
+
+public interface UserService extends UserDetailsService {
     User save(UserRegistrationDto registrationDto);
+    List<User> getAllUsers();
 }
